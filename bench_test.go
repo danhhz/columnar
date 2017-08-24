293fe33d1f6c84199b16fd7b6852828a1028a54c
Daniel Harrison <daniel.harrison@gmail.com>
2017-08-23 22:53:26 -0400

    switch to bare-bones encoding.binary version
    name             time/op
    UInt64/Encode-4    1.56µs ± 5%
    UInt64/Decode-4     769ns ± 7%
    UInt64/Both-4      2.34µs ± 6%
    
    name             speed
    UInt64/Encode-4  5.25GB/s ± 5%
    UInt64/Decode-4  10.7GB/s ± 7%
    UInt64/Both-4    3.51GB/s ± 6%
    
    name             alloc/op
    UInt64/Encode-4     0.00B
    UInt64/Decode-4     0.00B
    UInt64/Both-4       0.00B
    
    name             allocs/op
    UInt64/Encode-4      0.00
    UInt64/Decode-4      0.00
    UInt64/Both-4        0.00

--- bench_test.go
+++ bench_test.go
@@ -1,48 +1,49 @@
 package columnar
 
 import (
+	"encoding/binary"
 	"testing"
-
-	capnproto "zombiezen.com/go/capnproto2"
 )
 
 func BenchmarkUInt64(b *testing.B) {
-	setup := func(b *testing.B, size int) capnproto.UInt64List {
-		msg := &capnproto.Message{Arena: capnproto.SingleSegment(nil)}
-		seg, err := msg.Segment(0)
-		if err != nil {
-			b.Fatal(err)
-		}
-
-		l, err := capnproto.NewUInt64List(seg, int32(size))
-		if err != nil {
-			b.Fatal(err)
-		}
-		return l
-	}
-
 	b.Run("Encode", func(b *testing.B) {
-		l := setup(b, 1024)
+		buf := make([]byte, 8*1024)
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
 			for j := 0; j < 1024; j++ {
-				l.Set(j, uint64(j))
+				binary.LittleEndian.PutUint64(buf[j*8:j*8+8], uint64(j))
 			}
 		}
 		b.SetBytes(8 * 1024)
 	})
 
 	b.Run("Decode", func(b *testing.B) {
-		l := setup(b, 1024)
-			for j := 0; j < 1024; j++ {
-				l.Set(j, uint64(j))
-			}
+		buf := make([]byte, 8*1024)
+		for j := 0; j < 1024; j++ {
+			binary.LittleEndian.PutUint64(buf[j*8:j*8+8], uint64(j))
+		}
 		b.ResetTimer()
 
 		var x uint64
 		for i := 0; i < b.N; i++ {
 			for j := 0; j < 1024; j++ {
-				x += l.At(j)
+				x += binary.LittleEndian.Uint64(buf[j*8 : j*8+8])
+			}
+		}
+		b.SetBytes(8 * 1024)
+	})
+
+	b.Run("Both", func(b *testing.B) {
+		buf := make([]byte, 8*1024)
+		var x uint64
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			for j := 0; j < 1024; j++ {
+				binary.LittleEndian.PutUint64(buf[j*8:j*8+8], uint64(j))
+			}
+			for j := 0; j < 1024; j++ {
+				x += binary.LittleEndian.Uint64(buf[j*8 : j*8+8])
 			}
 		}
 		b.SetBytes(8 * 1024)
