fa3d100f89f0fd30ad576e74c0106ed6f98e0b46
Daniel Harrison <daniel.harrison@gmail.com>
2017-08-25 21:53:08 -0400

    locality and alignment benchmarks

--- bench_test.go
+++ bench_test.go
@@ -1,8 +1,11 @@
 package columnar
 
 import (
+	"crypto/rand"
 	"encoding/binary"
+	"strconv"
 	"testing"
+	"unsafe"
 )
 
 func BenchmarkUInt64(b *testing.B) {
@@ -47,5 +50,69 @@ func BenchmarkUInt64(b *testing.B) {
 			}
 		}
 		b.SetBytes(8 * 1024)
+	})
+}
+
+func BenchmarkLocality(b *testing.B) {
+	const max = 20
+	buf := make([]byte, 8*2<<max)
+	_, err := rand.Read(buf)
+	if err != nil {
+		b.Fatal(err)
+	}
+	for words := 1; words < 2<<max; words = words << 2 {
+		b.Run(strconv.Itoa(words), func(b *testing.B) {
+			b.Run("local", func(b *testing.B) {
+				var x uint64
+				for i := 0; i < b.N; i++ {
+					for j := 0; j < words; j++ {
+						x += binary.LittleEndian.Uint64(buf[0:8])
+					}
+				}
+				b.SetBytes(int64(8 * words))
+			})
+			b.Run("non-local", func(b *testing.B) {
+				var x uint64
+				for i := 0; i < b.N; i++ {
+					for j := 0; j < words; j++ {
+						x += binary.LittleEndian.Uint64(buf[j*8 : (j+1)*8])
+					}
+				}
+				b.SetBytes(int64(8 * words))
+			})
+		})
+	}
+}
+
+func BenchmarkAlignment(b *testing.B) {
+	var buf [15]byte
+	_, err := rand.Read(buf[0:])
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.Run("unsafe", func(b *testing.B) {
+		for offset := 0; offset < 8; offset++ {
+			b.Run(strconv.Itoa(offset), func(b *testing.B) {
+				var x uint64
+				for i := 0; i < b.N; i++ {
+					a := unsafe.Pointer(&buf[offset])
+					b := *(*uint64)(a)
+					x += b
+				}
+				b.SetBytes(8)
+			})
+		}
+	})
+	b.Run("encoding", func(b *testing.B) {
+		for offset := 0; offset < 8; offset++ {
+			b.Run(strconv.Itoa(offset), func(b *testing.B) {
+				var x uint64
+				for i := 0; i < b.N; i++ {
+					a := binary.LittleEndian.Uint64(buf[offset:])
+					x += a
+				}
+				b.SetBytes(8)
+			})
+		}
 	})
 }
